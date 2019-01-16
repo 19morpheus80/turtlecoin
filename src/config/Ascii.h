98eeed39822fc2dca4c7b085aa5275c58c1eea14
ExtraHash <ExtraHash@protonmail.com>
2019-01-15 23:37:08 -0700

    more mobile friendly ascii art

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -13,13 +13,40 @@ const std::string windowsAsciiArt =
       "   |_|\\__ _|_|   \\__|_|\\___|\\_____\\___/|_|_| |_|\n";
 
 const std::string nonWindowsAsciiArt = 
-      "\n                                                                            \n"
-        "████████╗██╗  ██╗██████╗ ████████╗██╗    ██████╗ █████╗ █████╗ ██╗███╗   ██╗\n"
-        "╚══██╔══╝██║  ██║██╔══██╗╚══██╔══╝██║    ██╔═══╝██╔═══╝██╔══██╗██║████╗  ██║\n"
-        "   ██║   ██║  ██║██████╔╝   ██║   ██║    ████╗  ██║    ██║  ██║██║██╔██╗ ██║\n"
-        "   ██║   ██║  ██║██╔══██╗   ██║   ██║    ██╔═╝  ██║    ██║  ██║██║██║╚██╗██║\n"
-        "   ██║   ╚█████╔╝██║  ██║   ██║   ██████╗██████╗╚█████╗╚█████╔╝██║██║ ╚████║\n"
-        "   ╚═╝    ╚════╝ ╚═╝  ╚═╝   ╚═╝   ╚═════╝╚═════╝ ╚════╝ ╚════╝ ╚═╝╚═╝  ╚═══╝\n";
+
+"\n                                                        \n"
+"MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMMMMMMMMMMMMNhsymMMMMMMMMMMMMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMMMMMMMMNho-     .+yNMMMMMMMMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMMMMMms:             -odMMMMMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMmyNMd`                oMMydMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMNy+.  `dMd`              yMN-  `/ymMMMMMMMMMMM \n"
+"MMMMMMMMh+-       `hMm.           `hMm.       ./yNMMMMMMM \n"
+"MMMMMMM+            sMN-         `dMd`           `MMMMMMM \n"
+"MMMMMMM+             sMMmddddddddNMd             .MMMMMMM \n"
+"MMMMMMMs            `dMdssoooooosdMN.            /MMMMMMM \n"
+"MMMMMMMd           `dMh`          oMN-           sMMMMMMM \n"
+"MMMMMMMM`         .mMs             /MN:          dMMMMMMM \n"
+"MMMMMMMM+.``````./NM+               -NM+.```````:MMMMMMMM \n"
+"MMMMMMMMMMMMMMMMMMMm                 yMMMMMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMo-````.-+NMs               /MMo-.````-/MMMMMMMMM \n"
+"MMMMMMMMMd        .mMy`            +MN:        yMMMMMMMMM \n"
+"MMMMMMMMMMo        `dMd`          sMN-        :MMMMMMMMMM \n"
+"MMMMMMMMMMM/        `dMmssooooooymMN.        .NMMMMMMMMMM \n"
+"MMMMMMMMMMMN-        hMMddhhhhhddNMd`       `mMMMMMMMMMMM \n"
+"MMMMMMMMMMMMN-      sMN-         `dMd`     .dMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMM/   `hMm.           `yMm.   -mMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMo .dMh`              sMN: /NMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMMhNMy                 +MMhMMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMMMMN`                  hMMMMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMMMMMd-               .yMMMMMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMMMMMMMh-           .sMMMMMMMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMMMMMMMMMh:       -sMMMMMMMMMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMMMMMMMMMMMm+` `/hMMMMMMMMMMMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMM \n"
+"MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM \n"
+
 
 /* Windows has some characters it won't display in a terminal. If your ascii
    art works fine on Windows and Linux terminals, just replace 'asciiArt' with
